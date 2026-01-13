e65c38cb2c83e4953027f90ebea5c62badd01a13
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2026-01-14 05:43:56 +0900

    CIのテスト

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { TestComponent } from "./Test";
 
-createRoot(document.getElementById("root")!).render(
+const root = document.querySelector<HTMLElement>("#root");
+
+if (!root) throw new Error("Failed to find the root element");
+
+createRoot(root).render(
   <StrictMode>
     <TestComponent />
   </StrictMode>,
