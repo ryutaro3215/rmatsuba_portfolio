cb994d4f86f6a81f3abc8cbf77c3e7037b3c7597
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2026-03-09 20:47:03 +0900

    make sitemap.xml and robot.txt at app/frontend/dist/client

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +19,12 @@ export default defineConfig({
         // 同時に処理するページ数を5に制限（Parallel処理の抑制）
         parallel: 5,
       },
+    }),
+    Sitemap({
+      hostname: "https://rmatsuba.com",
+      outDir: "dist/client",
+      exclude: ["/404"],
+      dynamicRoutes: ["/", "/about", "/blogs", "/books"],
     }),
   ],
 });
